7a6b8d52b238ef8b0573b8f5a1d160fafc3af0de
MaevaDjoko <maevadjoko9@gmail.com>
2025-12-16 10:52:22 +0100

    second commit

---
+++ ESIEE-IT_TP/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+interface Paiement
+{
+    void pay(float montant);
+}
+
+class Creditcard : Paiement
+{
+    string numerocarte;
+    public Creditcard(string NumeroCarte)
+    {
+        numerocarte = NumeroCarte;
+    }
+    public void pay(float montant)
+    {
+        Console.WriteLine($"Paiement de {montant} effectué par carte, {numerocarte}");
+    }
+}
+
+class Paypal : Paiement
+{
+    string email;
+    public Paypal(string Email)
+    {
+        email = Email;
+    }
+
+    public void pay(float montant)
+    {
+        Console.WriteLine($"Paiement de {montant} effectué par paypal {email}");
+    }
+}
+
+class PaiementLivraison : Paiement
+{
+    public void pay(float montant)
+    {
+        Console.WriteLine($"Paiement de {montant} à la livraison");
+    }
+}
+class Commande
+{
+    Paiement typepaiement;
+    float montant;
+
+    public Commande(Paiement TypePaiment, float Montant)
+    {
+        typepaiement = TypePaiment;
+        montant = Montant;
+    }
+
+    public void checkout()
+    {
+        typepaiement.pay(montant);
+    }
+}
+
+class Programme
+{
+    static void Main()
+    {
+        Commande CI = new Commande(new Paypal("Test@gmail.com"), 200.0f);
+        CI.checkout();
+    }
+}
